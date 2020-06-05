bf828707fdce1f65a83bfd7ceabbd06bce765644
Tomasz <tomasz.milanicz@gmail.com>
2020-06-05 22:11:07 +0200

    testing room list display
    * attempts to generate content for existing rooms
       - all attributes of the "room" record displayed
       - so far the form is poor, it's about presenting possibilities
       - template generation with read content in ES6 form
       - ... after a few updates of the correct syntax to use
       - styles for interaction were built quickly
    * attempts to add interactive elements to control the booking of the selected room
       - for now they only exist with minimal names and connections
       - no logic for interactivity!

--- src/views/oops-404.js
+++ src/views/oops-404.js
@@ -3,12 +3,12 @@
 import $ from 'jquery';
 
 export const oops404 = () => {
-    const fragment = $(new DocumentFragment);   // (+) WYDAJNOŚĆ:
+    const $fragment = $(new DocumentFragment() );   // (+) WYDAJNOŚĆ:
 //...brak ciągłych operacji na DOM, przy kolejnej operacji na każdym z nowo dostawianych z osobna elementów lub ich modyfikowaniu (np. atrybuty, itp.)
 
-    fragment
-        .append('<h2>OopS!</h2>')
-        .append('<p class="error">Nie ma takiego adresu! To się miało nigdy nie zdarzyć, ale jednak niemożliwe staje się osiągalne.<br /><strong>Użytkowniku</strong>: <em>nie kobinuj! Zweryfikuj wprowadzony adres lub korzystaj tylko z wyświetlonych odnośników i przycisków!</em></p>');
+    $fragment
+        .append('<h2>OopS! 404</h2>')
+        .append('<p class="error">Nie ma takiego adresu! To się miało nigdy nie zdarzyć, ale jednak niemożliwe staje się osiągalne.<br /><strong>Użytkowniku</strong>: <em>nie kombinuj! Zweryfikuj wprowadzony adres lub korzystaj tylko z wyświetlonych odnośników i przycisków!</em></p>');
 
-    return fragment;
+    return $fragment;
 };
--- src/views/rooms-list-item.js
+++ src/views/rooms-list-item.js
@@ -2,8 +2,80 @@
 
 import $ from 'jquery';
 
-export const roomsListItem = ( room ) => {
-    const li = $('<li class="list-group-item"></li>');
-    li.text( room.name ); // póki co zwrot jako sam tekst w elemencie, zawierający tylko nazwę pokoju
-    return li;
+export const roomsListItem = ( anyRoom ) => {
+    const $li = $('<li class="list-group-item"></li>');
+
+    const $fragment = $( new DocumentFragment() );
+
+    // #1 LIPA
+    /*      fragment
+      //.append('<div></div>')
+      .append('<h4></h4>', { className: "room-name" }).text( anyRoom.name )
+      .append('<h5></h5>', { className: "price" }).text( "cena: " + anyRoom.price );
+ */
+
+    // #2 LEPIEJ... ale rozbite na tworzenie struktury, by potem ją wuypełnić treścią bądź zmodyfikwać
+/*       $fragment.append('<h4 class="room-name"></h4><h5 class="price"></h5>');
+      $fragment.find('h4').text( anyRoom.name )
+        .parent().find('h5').text( "cena: " + anyRoom.price );
+ */
+
+    // #3 TWORZENIE DYNAMICZNE ELEMNETÓW STRUKTURY JAKO TABLICA ELEMENTÓW
+        // jest niemal świetne, ale trzeba pamiętać o zamykaniu elementów, przy ich zagnieżdżaniu
+/*     const $myNewRoom = $( [
+        '  <h4 class="room-name">',
+        '    ' + anyRoom.name,
+        '  </h4>',
+        '  <h5 class="price">',
+        '    Cena: ' + anyRoom.price + 'zł',
+        '  </h5>',
+        ].join("\n") );
+    // console.log( $myNewRoom );
+
+    $fragment.append( $myNewRoom );
+ */
+
+    // #4 UŻYCIE ES6 do budowy struktury jak powyżej.. albo każdej innej LEPSZEJ
+    const $myNewRoomHTML = `
+        <h4 class="room-name">
+            <string>${anyRoom.name}</strong> (<span title="nr identyfikatora">#${anyRoom.id}</span>)
+        </h4>
+        <h5>
+            Ilość łóżek: <strong>${anyRoom.beds}</strong><br />
+            Miejsc dla gości: <strong>${anyRoom.guests}</strong> 
+        </h5>
+        <h5 class="price">
+            Cena: <strong>${anyRoom.price}<strong> zł za każdy dzień
+        </h5>
+        <div class="room-booking">
+            <form id="room-booking">
+                <label for="book-date-from">Data od: </label>
+                <input type="date" name="book-date-from" id="book-date-from" min="2020-04-01" max="2021-04-01">
+                <label for="book-room-days">Ile dni: </label> 
+                <input type="number" name="book-room-days" id="book-room-days" min="0" max="14" maxlength="3" />
+                <label for="book-date-to">
+                Data do: <input type="date" name="book-date-to" id="book-date-to" min="2020-04-01" max="2021-04-01">
+                <button id="book-room" class="btn btn-primary" data-room-id="${anyRoom.id}">Rezerwuję ten pokój (${anyRoom.id})</button>
+            </form>
+        </div>    
+    `;
+
+    $fragment.append( $myNewRoomHTML );
+
+/*     this.$fixture = $([
+        "<div>",
+        "  <div class='js-alert-box'></div>",
+        "  <form id='my-form-to-validate'>",
+        "    <input id='login-username' name='login-username'>",
+        "  </form>",
+        "</div>"
+        ].join("\n"));
+ */
+      // return fragment;
+    //li.text( room.name ); // póki co zwrot jako sam tekst w elemencie, zawierający tylko nazwę pokoju
+    $li.append( $fragment );
+ 
+    // li.text( "ELEMENT " + room.name ); // póki co zwrot jako sam tekst w elemencie, zawierający tylko nazwę pokoju
+
+    return $li;
 };
